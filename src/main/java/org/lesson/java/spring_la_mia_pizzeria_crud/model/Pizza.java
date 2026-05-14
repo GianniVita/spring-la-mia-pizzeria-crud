deb050a5caf5158a6a88be8c01fa229416dbf4e8
giannivitale <giavitale78@gmail.com>
2026-05-14 13:40:35 +0200

    Initialize Spring La Mia Pizzeria CRUD application with basic structure, including Maven configuration, application properties, and initial Pizza entity with controller and view.

---
+++ src/main/java/org/lesson/java/spring_la_mia_pizzeria_crud/model/Pizza.java
@@ -0,0 +1,41 @@
+package org.lesson.java.spring_la_mia_pizzeria_crud.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "pizzas")
+public class Pizza {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank(message = "NAME must not be null, nor empty or blank")
+    @Size(min = 2, max = 120, message = "The name of the pizza must be at list of two caracters")
+    @Column(name = "nome_della_pizza", nullable = false)
+    private String name;
+
+    @NotBlank(message = "NAME must not be null, nor empty or blank")
+    @Size(min = 50, max = 500)
+    @Column(name = "pizza_description")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    @Column(nullable = false)
+    private BigDecimal price;
+
+    @Size(max = 500, message = "Photo URL is too long")
+    @Column(name = "photo_url")
+    private String photo;
+
+}
